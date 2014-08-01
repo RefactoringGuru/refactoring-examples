3a80321d3c8af0d03d7768d779457e74fae41769
neochief <neochief@shvetsgroup.com>
2014-08-01 15:14:09 +0300

    Fixed few typos in python examples.

--- simple/python/extract-class_before.py
+++ simple/python/extract-class_before.py
@@ -1,10 +1,10 @@
 class Soldier:
-    self. health = 0
+    self.health = 0
     self.damage = 0
     self.weaponStatus = 0
 
     def getDamage(self):
-        #... 
-   
+        #...
+
     def attack(self):
-        #... 
+        #...
--- simple/python/replace-method-with-method-object_before.py
+++ simple/python/replace-method-with-method-object_before.py
@@ -1,7 +1,7 @@
 class Order:
-#...
+    #...
     def price(self):
-	primaryBasePrice = 0
+        primaryBasePrice = 0
         secondaryBasePrice = 0
         tertiaryBasePrice = 0
         # long computation.
