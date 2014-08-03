05468e055da16a0929533ba9acd9a23ad508bcc0
freehex <askme@gmail.com>
2014-08-04 00:18:03 +0400

    Bug fixed: in Assert.IsTrue condition "||" is replaced by "&&"

--- simple/csharp/introduce-assertion_after.cs
+++ simple/csharp/introduce-assertion_after.cs
@@ -1,6 +1,6 @@
 double GetExpenseLimit() 
 {
-  Assert.IsTrue(expenseLimit != NULL_EXPENSE || primaryProject != null);
+  Assert.IsTrue(expenseLimit != NULL_EXPENSE && primaryProject != null);
 
   return expenseLimit != NULL_EXPENSE ?
     expenseLimit:
