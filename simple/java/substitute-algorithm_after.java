d1b2f580c84bec9924c35867a30ad269f7041d63
Alexander Shvets <neochief@shvetsgroup.com>
2015-08-12 11:23:22 +0300

    Fixed typo in code.

--- simple/java/substitute-algorithm_after.java
+++ simple/java/substitute-algorithm_after.java
@@ -1,7 +1,7 @@
 String foundPerson(String[] people){
   List candidates =
     Arrays.asList(new String[] {"Don", "John", "Kent"});
-  for (int i=0; i<people.length; i++) {
+  for (int i=0; i < people.length; i++) {
     if (candidates.contains(people[i])) {
       return people[i];
     }
