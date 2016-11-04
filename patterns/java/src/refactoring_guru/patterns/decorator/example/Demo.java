1061c47e188e749dd21895e9b086b697798f8973
b1ger <b.gerashchencko@gmail.com>
2016-11-04 10:03:57 +0200

    Delete Demo.java

--- patterns/java/src/refactoring_guru/patterns/decorator/example/Demo.java
+++
@@ -1,8 +0,0 @@
-package refactoring_guru.patterns.decorator.example;
-
-public class Demo {
-    public static void main(String[] args) {
-        ApplicationConfiguration app = new ApplicationConfiguration(true, false);
-        app.configurationExample();
-    }
-}
