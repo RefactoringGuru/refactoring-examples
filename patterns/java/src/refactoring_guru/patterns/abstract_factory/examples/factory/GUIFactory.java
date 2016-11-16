1d8080a8708601d96456b9605ba327127fe6d353
b1ger <b.gerashchencko@gmail.com>
2016-11-16 11:52:49 +0200

    Delete GUIFactory.java

--- patterns/java/src/refactoring_guru/patterns/abstract_factory/examples/factory/GUIFactory.java
+++
@@ -1,9 +0,0 @@
-package refactoring_guru.patterns.abstract_factory.examples.factory;
-
-import refactoring_guru.patterns.abstract_factory.examples.button.Button;
-import refactoring_guru.patterns.abstract_factory.examples.checkbox.Checkbox;
-
-public interface GUIFactory {
-    public abstract Button createButton();
-    public abstract Checkbox createCheckbox();
-}
