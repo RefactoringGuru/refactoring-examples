13f4b61722dba041cf4715d1920e32a5dfbb4f75
b1ger <b.gerashchencko@gmail.com>
2016-12-01 12:43:07 +0200

    edit dot2

--- patterns/java/src/refactoring_guru/patterns/composite/example/graphics/Dot.java
+++ patterns/java/src/refactoring_guru/patterns/composite/example/graphics/Dot.java
@@ -25,15 +25,11 @@ public class Dot extends Canvas implements Graphic {
 
     @Override
     public void draw() {
-<<<<<<< HEAD
         JFrame frame = new JFrame("Dot");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(x + DOT_SIZE, y + DOT_SIZE);
         frame.add(this);
         frame.setVisible(true);
-=======
-        JFrame frame = getFrame(x + mn, y + mn, component);
->>>>>>> eaf0f8dc5bfdf95180459d674f648d2cac88c440
     }
 
     @Override
