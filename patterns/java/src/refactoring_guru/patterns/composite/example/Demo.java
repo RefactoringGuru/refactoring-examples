af6c1e55ca0637a54ab542e7c043977e795b1d5f
Alexander Shvets <neochief@shvetsgroup.com>
2016-12-08 18:45:02 +0200

    Composite implementation in Java.

---
+++ patterns/java/src/refactoring_guru/patterns/composite/example/Demo.java
@@ -0,0 +1,28 @@
+package refactoring_guru.patterns.composite.example;
+
+import refactoring_guru.patterns.composite.example.shapes.*;
+
+import java.awt.Color;
+
+public class Demo {
+    public static void main(String[] args) {
+        ImageEditor editor = new ImageEditor();
+
+        editor.loadShapes(
+                new Circle(10, 10, 10, Color.BLUE),
+
+                new CompoundShape(
+                    new Circle(110, 110, 50, Color.RED),
+                    new Dot(160, 160, Color.RED)
+                ),
+
+                new CompoundShape(
+                        new Rectangle(250, 250, 100, 100, Color.GREEN),
+                        new Dot(240, 240, Color.GREEN),
+                        new Dot(240, 360, Color.GREEN),
+                        new Dot(360, 360, Color.GREEN),
+                        new Dot(360, 240, Color.GREEN)
+                )
+        );
+    }
+}
---
+++ patterns/java/src/refactoring_guru/patterns/composite/example/ImageEditor.java
@@ -0,0 +1,76 @@
+package refactoring_guru.patterns.composite.example;
+
+import refactoring_guru.patterns.composite.example.shapes.Shape;
+import refactoring_guru.patterns.composite.example.shapes.CompoundShape;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+class ImageEditor {
+    private EditorCanvas canvas;
+    private CompoundShape allShapes = new CompoundShape();
+
+    ImageEditor() {
+        canvas = new EditorCanvas();
+    }
+
+    void loadShapes(Shape... shapes) {
+        allShapes.clear();
+        allShapes.add(shapes);
+        canvas.refresh();
+    }
+
+    private class EditorCanvas extends Canvas {
+        JFrame frame;
+
+        private static final int PADDING = 10;
+
+        EditorCanvas() {
+            createFrame();
+            refresh();
+            addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    allShapes.unSelect();
+                    allShapes.selectChildAt(e.getX(), e.getY());
+                    e.getComponent().repaint();
+                }
+            });
+        }
+
+        void createFrame() {
+            frame = new JFrame();
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setLocationRelativeTo(null);
+
+            JPanel contentPanel = new JPanel();
+            Border padding = BorderFactory.createEmptyBorder(PADDING, PADDING, PADDING, PADDING);
+            contentPanel.setBorder(padding);
+            frame.setContentPane(contentPanel);
+
+            frame.add(this);
+            frame.setVisible(true);
+            frame.getContentPane().setBackground(Color.LIGHT_GRAY);
+        }
+
+        public int getWidth() {
+            return allShapes.getX() + allShapes.getWidth() + PADDING;
+        }
+
+        public int getHeight() {
+            return allShapes.getY() + allShapes.getHeight() + PADDING;
+        }
+
+        void refresh() {
+            this.setSize(getWidth(), getHeight());
+            frame.pack();
+        }
+
+        public void paint(Graphics graphics) {
+            allShapes.paint(graphics);
+        }
+    }
+}
