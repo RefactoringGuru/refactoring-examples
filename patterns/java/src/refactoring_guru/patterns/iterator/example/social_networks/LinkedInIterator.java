4257369add7ba0e5856bff36a35363d3118dc5aa
b1ger <b.gerashchencko@gmail.com>
2016-12-28 17:01:06 +0200

    Iterator implementation in Java
    * Complete abstract factory
    
    * del
    
    * Delete Demo.java
    
    * Delete Application.java
    
    * Delete Button.java
    
    * Delete OSXButton.java
    
    * Delete WinButton.java
    
    * Delete Checkbox.java
    
    * Delete OSXCheckbox.java
    
    * Delete WinCheckbox.java
    
    * Delete GUIFactory.java
    
    * Delete OSXFactory.java
    
    * Delete WinFactory.java
    
    * Add broken iterator
    
    * Update Demo.java
    
    * Edit iterator java
    
    * Edit iterator pettern example
    
    * edit iterators
    
    * edit networks and iterarors

---
+++ patterns/java/src/refactoring_guru/patterns/iterator/example/social_networks/LinkedInIterator.java
@@ -0,0 +1,43 @@
+package refactoring_guru.patterns.iterator.example.social_networks;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LinkedInIterator implements ProfileIterator {
+    private LinkedIn linkedIn;
+    private String email;
+    private int currentPosition;
+    private Map<Integer, Profile> cache = new HashMap<>();
+
+    public LinkedInIterator(LinkedIn linkedIn, String email) {
+        this.linkedIn = linkedIn;
+        this.email = email;
+    }
+
+    private void lazyLoad() {
+        if (cache.size() == 0) {
+            List<Profile> profiles = linkedIn.getProfile(this.email).getCoworkers();
+            int index = 1;
+            for (Profile profile : profiles) {
+                cache.put(index, profile);
+                index++;
+            }
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        lazyLoad();
+        return cache.size() > currentPosition;
+    }
+
+    @Override
+    public Profile getNext() {
+        if (hasNext()) {
+            currentPosition++;
+            return cache.get(currentPosition);
+        }
+        return null;
+    }
+}
