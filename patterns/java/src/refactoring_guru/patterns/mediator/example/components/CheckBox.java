b0fa64eada293b94224726f7df5a4dc17a040d4f
b1ger <b.gerashchencko@gmail.com>
2017-01-31 20:04:53 +0200

    implemented mediator pattern java

---
+++ patterns/java/src/refactoring_guru/patterns/mediator/example/components/CheckBox.java
@@ -0,0 +1,9 @@
+package refactoring_guru.patterns.mediator.example.components;
+
+import refactoring_guru.patterns.mediator.example.Mediator;
+
+public class CheckBox extends Component {
+    public CheckBox(Mediator mediator) {
+        super(mediator);
+    }
+}
