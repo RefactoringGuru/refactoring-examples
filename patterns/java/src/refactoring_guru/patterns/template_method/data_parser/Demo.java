7f64c0f3182e00164982c290f5a9fbba017b2c7b
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-02-22 11:32:36 +0200

    New example for Template method pattern

---
+++ patterns/java/src/refactoring_guru/patterns/template_method/data_parser/Demo.java
@@ -0,0 +1,8 @@
+package refactoring_guru.patterns.template_method.data_parser;
+
+public class Demo {
+    public static void main(String[] args) {
+        CSVDataParser dataParser = new CSVDataParser();
+        dataParser.parseDataAndOutput();
+    }
+}
