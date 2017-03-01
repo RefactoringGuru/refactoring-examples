9ba1c26157e3b4b6054cefeb378751f2afb5c200
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-03-01 16:18:07 +0200

    Edit backup method

--- patterns/java/src/refactoring_guru/patterns/memento/drag_image/editor/Memento.java
+++ patterns/java/src/refactoring_guru/patterns/memento/drag_image/editor/Memento.java
@@ -1,12 +1,13 @@
 package refactoring_guru.patterns.memento.drag_image.editor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Memento {
     List backup;
 
     public void setBackup(List list) {
-        backup = list;
+        this.backup = new ArrayList(list);
     }
 
     public List restore() {
