a18b94201606dd037e2ca4dbb3b23efe98a72c38
b1ger <b.gerashchencko@gmail.com>
2017-03-06 12:57:40 +0200

    New example for Memento pattern (#66)
    * Add new example for Memento pattern
    
    * Edit package name
    
    * Edit backup method
    
    * Edit additional example for Memento pattern

---
+++ patterns/java/src/refactoring_guru/patterns/memento/drag_image/MenuPad.java
@@ -0,0 +1,50 @@
+package refactoring_guru.patterns.memento.drag_image;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+
+public class MenuPad extends MouseAdapter {
+    private JPopupMenu menu = new JPopupMenu();
+    private ShapesDragging shapesDragging;
+    private Memento backup;
+
+    public MenuPad(ShapesDragging shapesDragging, Memento backup) {
+        this.shapesDragging = shapesDragging;
+        this.backup = backup;
+        initMenu();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON3) {
+            menu.show(e.getComponent(), e.getX(), e.getY());
+        }
+    }
+
+    public void initMenu() {
+        JMenuItem circle = new JMenuItem("Add circle");
+        circle.addActionListener(actionEvent -> {
+            backup.setBackup(shapesDragging.items);
+            shapesDragging.addItem(new Ellipse2D.Double(80, 40, 80, 80), Color.CYAN);
+            shapesDragging.repaint();
+        });
+        menu.add(circle);
+
+        JMenuItem rectangle = new JMenuItem("Add rectangle");
+        rectangle.addActionListener(actionEvent -> {
+            backup.setBackup(shapesDragging.items);
+            shapesDragging.addItem(new Rectangle2D.Double(40, 60, 50,50), Color.MAGENTA);
+            shapesDragging.repaint();
+        });
+        menu.add(rectangle);
+
+        JMenuItem undo = new JMenuItem("Undo last added");
+        undo.addActionListener(actionEvent -> {
+            shapesDragging.items = backup.restore();
+            shapesDragging.repaint();
+        });
+        menu.add(undo);
+    }
+}
