8112154c3c10d975cf491df0bb602ef124e584c4
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-03-13 09:34:19 +0200

    Add Demo class and OutputDemo

---
+++ patterns/java/src/refactoring_guru/patterns/memento/drag_image/Demo.java
@@ -0,0 +1,8 @@
+package refactoring_guru.patterns.memento.drag_image;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
