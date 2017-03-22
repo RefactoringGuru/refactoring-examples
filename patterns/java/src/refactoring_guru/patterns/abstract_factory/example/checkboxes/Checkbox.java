b520f730ca100f6f20fd1c8a239df6aac0a0c150
Alexander Shvets <neochief@shvetsgroup.com>
2017-03-22 21:08:08 +0200

    Fixed comments in abstract factory.

--- patterns/java/src/refactoring_guru/patterns/abstract_factory/example/checkboxes/Checkbox.java
+++ patterns/java/src/refactoring_guru/patterns/abstract_factory/example/checkboxes/Checkbox.java
@@ -1,9 +1,11 @@
 package refactoring_guru.patterns.abstract_factory.example.checkboxes;
 
 /**
- * EN: Checkboxes is the second product family. It has the same variants as buttons.
- *
- * RU: Чекбоксы — это второе семейство продуктов. Оно имеет те же вариации, что и кнопки.
+ * EN: Checkboxes is the second product family. It has the same variants
+ * as buttons.
+ * 
+ * RU: Чекбоксы — это второе семейство продуктов. Оно имеет те же вариации, что
+ * и кнопки.
  */
 public interface Checkbox {
     public abstract void paint();
