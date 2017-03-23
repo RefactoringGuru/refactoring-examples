3341ad97b3c65fd2c1481900f79544977f838770
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-03-23 16:14:00 +0200

    Add new cards with loops for long method smell

---
+++ simple/java/decompose-loop-after.java
@@ -0,0 +1,11 @@
+void printProperties(List users) {
+    for (User user : users) {
+        System.out.println(getProperties(user));
+
+        // ...
+    }
+}
+
+String getProperties(User user)  {
+    return user.getName() + " " + user.getAge();
+}
---
+++ simple/java/decompose-loop-before.java
@@ -0,0 +1,9 @@
+void printProperties(List users) {
+    for (int i = 0; i < users.size(); i++) {
+        String result = null;
+        result += users.get(i).getName();
+        result += users.get(i).getage();
+        System.out.println(result);
+        // ...
+    }
+}
