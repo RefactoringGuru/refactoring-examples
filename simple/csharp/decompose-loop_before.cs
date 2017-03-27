5dbcd467e9b887d783ccf1413d107f83f61f4898
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-03-27 16:57:05 +0300

    Edit examples for long method smell

--- simple/csharp/decompose-loop_before.cs
+++ simple/csharp/decompose-loop_before.cs
@@ -1,6 +1,6 @@
     void printProperties(IList users) {
         for (int i = 0; i < users.size(); i++) {
-            string result = null;
+            string result = "";
             result += users.get(i).getName();
             result += users.get(i).getage();
             Console.WriteLine(result);
--- simple/java/decompose-loop_before.java
+++ simple/java/decompose-loop_before.java
@@ -1,6 +1,6 @@
     void printProperties(List users) {
         for (int i = 0; i < users.size(); i++) {
-            String result = null;
+            String result = "";
             result += users.get(i).getName();
             result += users.get(i).getage();
             System.out.println(result);
