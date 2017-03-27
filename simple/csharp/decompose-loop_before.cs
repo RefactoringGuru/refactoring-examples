5a17e9ebe3e993b1a24161b7d45c855e046e8855
b1ger <b.gerashchencko@gmail.com>
2017-03-27 20:00:59 +0300

    Update decompose-loop_before.cs

--- simple/csharp/decompose-loop_before.cs
+++ simple/csharp/decompose-loop_before.cs
@@ -2,11 +2,11 @@ void printProperties(IList users)
 {
   for (int i = 0; i < users.size(); i++) 
   {
-  string result = "";
-  result += users.get(i).getName();
-  result += users.get(i).getage();
-  Console.WriteLine(result);
+    string result = "";
+    result += users.get(i).getName();
+    result += users.get(i).getage();
+    Console.WriteLine(result);
 
-  // ...
+    // ...
   }
 }
