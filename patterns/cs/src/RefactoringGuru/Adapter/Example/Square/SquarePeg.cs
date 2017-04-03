6c6d82d3f0f838d7739683128692a7f7c06f584f
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-04-03 14:07:55 +0300

    Add first CSharp example, Adapter pattern

---
+++ patterns/cs/src/RefactoringGuru/Adapter/Example/Square/SquarePeg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Adapter.Example.Square
+{
+    /**
+     * EN: SquarePegs are not compatible with RoundHoles (they were implemented by
+     * previous development team). But we have to integrate them into our program.
+     * 
+     * RU: КвадратныеКолышки несовместимы с КруглымиОтверстиями (они остались в
+     * проекте после бывших разработчиков). Но мы должны как-то интегрировать их в
+     * нашу систему.
+     */
+    class SquarePeg
+    {
+        private double width;
+
+        public SquarePeg(double width)
+        {
+            this.width = width;
+        }
+
+        public double getWidth()
+        {
+            return width;
+        }
+
+        public double getSquare()
+        {
+            double result;
+            result = Math.Pow(this.width, 2);
+            return result;
+        }
+    }
+}
