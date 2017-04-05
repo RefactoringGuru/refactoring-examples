7dc945c71dd2891cc339e4875533ecc46fdb36ab
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-04-05 12:28:30 +0300

    Add Abstract factory in csharp

---
+++ patterns/cs/src/RefactoringGuru/AbstractFactory/Example/Buttons/MacOSButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RefactoringGuru.AbstractFactory.Example.Buttons
+{
+    /**
+     * EN: All products families have the same varieties (MacOS/Windows).
+     * 
+     * This is a MacOS variant of a button.
+     * 
+     * RU: Все семейства продуктов имеют одни и те же вариации (MacOS/Windows).
+     * 
+     * Это вариант кнопки под MacOS.
+     */
+    class MacOsButton : IButton
+    {
+        public void Paint()
+        {
+            Console.WriteLine("You created MacOSButton.");
+        }
+    }
+}
---
+++ patterns/cs/src/RefactoringGuru/AbstractFactory/Example/Factories/WindowsFactory.cs
@@ -0,0 +1,24 @@
+﻿using RefactoringGuru.AbstractFactory.Example.Buttons;
+using RefactoringGuru.AbstractFactory.Example.Checkboxes;
+
+namespace RefactoringGuru.AbstractFactory.Example.Factories
+{
+    /**
+     * EN: Each concrete factory extends basic factory and responsible for creating
+     * products of a single variety.
+     * 
+     * RU: Каждая конкретная фабрика знает и создаёт только продукты своей вариации.
+     */
+    class WindowsFactory : IGuiFactory
+    {
+        public IButton CreateButton()
+        {
+            return new WindowsButton();
+        }
+
+        public ICheckbox CreateCheckbox()
+        {
+            return new WindowsCheckbox();
+        }
+    }
+}
