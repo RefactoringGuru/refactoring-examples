fec7537b98799b4b00626befa724de4cf0c655b0
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-04-05 12:28:30 +0300

    Add Abstract factory in csharp

---
+++ patterns/cs/src/RefactoringGuru/AbstractFactory/Example/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using RefactoringGuru.AbstractFactory.Example.App;
+using RefactoringGuru.AbstractFactory.Example.Factories;
+
+namespace RefactoringGuru.AbstractFactory.Example
+{
+    /**
+     * EN: Demo class. Everything comes together here.
+     * 
+     * RU: Демо-класс. Здесь всё сводится воедино.
+     */
+    class Program
+    {
+        /**
+         * EN: Application picks the factory type and creates it in run time
+         * (usually at initialization stage), depending on the configuration or
+         * environment variables.
+         * 
+         * RU: Приложение выбирает тип и создаёт конкретные фабрики динамически
+         * исходя из конфигурации или окружения.
+         */
+        private static Application ConfigureApplication()
+        {
+            Application app;
+            IGuiFactory factory;
+            String osName = System.Environment.GetEnvironmentVariable("OS").ToLower();
+            if (osName.Contains("mac"))
+            {
+                factory = new MacOSFactory();
+                app = new Application(factory);
+            }
+            else
+            {
+                factory = new WindowsFactory();
+                app = new Application(factory);
+            }
+            return app;
+        }
+
+        static void Main(string[] args)
+        {
+            Application app = ConfigureApplication();
+            app.Paint();
+        }
+    }
+}
