4ad3d58aebdcf2f0e4511c20b323c11e25752ee5
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-04-05 14:25:05 +0300

    Add Prototype in csharp

---
+++ patterns/cs/src/RefactoringGuru/Prototype/PrototypeExample/Example/Shapes/Shape.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RefactoringGuru.PrototypeEample.Example.Shapes
+{
+    abstract class Shape
+    {
+        public int x;
+        public int y;
+        public String color;
+
+        public Shape() { }
+
+        public Shape(Shape target)
+        {
+            if (target != null)
+            {
+                this.x = target.x;
+                this.y = target.y;
+                this.color = target.color;
+            }
+        }
+
+        public abstract Shape Clone();
+        
+        public bool Equals(Object object2)
+        {
+            if (!(object2.GetType() == typeof(Shape)))
+            {
+                return false;
+            }
+            Shape shape2 = (Shape) object2;
+            return shape2.x == x && shape2.y == y && shape2.color == color;
+        }
+    }
+}
