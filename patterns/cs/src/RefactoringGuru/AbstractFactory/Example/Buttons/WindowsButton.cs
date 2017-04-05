53533b11350bd4bc9093395ca0d162d78caeb4e1
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-04-05 10:10:07 +0300

    Add Abstract Factory in csharp

---
+++ patterns/cs/src/RefactoringGuru/AbstractFactory/Example/Buttons/WindowsButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RefactoringGuru.AbstractFactory.Example.Buttons
+{
+    /**
+     * EN: All products families have the same varieties (MacOS/Windows).
+     * 
+     * This is another variant of a button.
+     * 
+     * RU: Все семейства продуктов имеют одни и те же вариации (MacOS/Windows).
+     * 
+     * Это вариант кнопки под Windows.
+     */
+    class WindowsButton : IButton
+    {
+        public void Paint()
+        {
+            Console.WriteLine("You create WindowsButton.");
+        }
+    }
+}
