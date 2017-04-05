74ccdcfd66286e0fd8be0cd1aae10aa99dd65d38
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-04-05 13:35:44 +0300

    Factory method (#84)
    * Add Factory method in csharp
    
    * delete .resx file

---
+++ patterns/cs/src/RefactoringGuru/FactoryMethod/Example/Buttons/IButton.cs
@@ -0,0 +1,13 @@
+﻿namespace RefactoringGuru.FactoryMethod.Example.Buttons
+{
+    /**
+     * EN: Common interface for all buttons.
+     * 
+     * RU: Общий интерфейс для всех продуктов.
+     */
+    interface IButton
+    {
+        void Render();
+        void OnClick();
+    }
+}
