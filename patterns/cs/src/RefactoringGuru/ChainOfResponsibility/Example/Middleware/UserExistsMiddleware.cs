3d864b7ba47957c6b24cafd8285e98029e8eb083
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-04-07 11:35:26 +0300

    Add chain of responsibility example in csharp

---
+++ patterns/cs/src/RefactoringGuru/ChainOfResponsibility/Example/Middleware/UserExistsMiddleware.cs
@@ -0,0 +1,18 @@
+﻿namespace RefactoringGuru.ChainOfResponsibility.Example.Middleware
+{
+    class UserExistsMiddleware : Middleware
+    {
+
+        /**
+         * RU: Конкретный элемент цепи обрабатывает запрос по-своему.
+         */
+        public override bool Check(string email, string password)
+        {
+            if (!Server.HasEmail(email))
+            {
+                return false;
+            }
+            return CheckNext(email, password);
+        }
+    }
+}
