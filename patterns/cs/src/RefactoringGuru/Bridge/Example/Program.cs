3c83c382f249c12a88251408313851f1c8edf3be
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-04-07 15:41:21 +0300

    Add bridge example in csharp (#90)

---
+++ patterns/cs/src/RefactoringGuru/Bridge/Example/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RefactoringGuru.Bridge.Example.Devices;
+using RefactoringGuru.Bridge.Example.Remotes;
+using RefactoringGuruBridge.Example.Devices;
+using RefactoringGuruBridge.Example.Remotes;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestDevice(new TV());
+            TestDevice(new Radio());
+
+            Console.ReadKey();
+        }
+
+        public static void TestDevice(IDevice device)
+        {
+            Console.WriteLine("Tests with basic remote.");
+            BasicRemote basicRemote = new BasicRemote(device);
+            basicRemote.Power();
+            device.PrintStatus();
+
+            Console.WriteLine("Tests with advanced remote.");
+            AdvancedRemote advancedRemote = new AdvancedRemote(device);
+            advancedRemote.Power();
+            advancedRemote.Mute();
+            device.PrintStatus();
+        }
+    }
+}
---
+++ patterns/cs/src/RefactoringGuru/Bridge/Example/Remotes/BasicRemote.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using RefactoringGuruBridge.Example.Devices;
+
+namespace RefactoringGuru.Bridge.Example.Remotes
+{
+    class BasicRemote : IRemote
+    {
+        protected IDevice device;
+
+        public BasicRemote() { }
+
+        public BasicRemote(IDevice device)
+        {
+            this.device = device;
+        }
+
+        public void Power()
+        {
+            Console.WriteLine("Remote: power toggle");
+            if (device.IsEnabled())
+            {
+                device.Disable();
+            }
+            else
+            {
+                device.Enable();
+            }
+        }
+
+        public void VolumeUp()
+        {
+            Console.WriteLine("Remote: volume up");
+            device.SetVolume(device.GetVolume() + 10);
+        }
+
+        public void VolumeDown()
+        {
+            Console.WriteLine("Remote: volume down");
+            device.SetVolume(device.GetVolume() - 10);
+        }
+
+        public void ChannelDown()
+        {
+            Console.WriteLine("Remote: channel down");
+            device.SetChannel(device.GetChannel() - 1);
+        }
+
+        public void ChannelUp()
+        {
+            Console.WriteLine("Remote: channel up");
+            device.SetChannel(device.GetChannel() + 1);
+        }
+    }
+}
