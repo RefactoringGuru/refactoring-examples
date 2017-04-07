ef6594ed0eafdb0e06a291d68d78697b31c6426b
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-04-07 15:38:28 +0300

    Factory (#89)
    * Add empty projects
    
    * Add Abstract factory in csharp
    
    * Add Abstract factory in csharp

---
+++ patterns/cs/src/RefactoringGuru/AbstractFactory/Example/Checkboxes/Checkbox.cs
@@ -0,0 +1,14 @@
+﻿namespace RefactoringGuru.AbstractFactory.Example.Checkboxes
+{
+    /**
+     * EN: Checkboxes is the second product family. It has the same variants
+     * as buttons.
+     * 
+     * RU: Чекбоксы — это второе семейство продуктов. Оно имеет те же вариации, что
+     * и кнопки.
+     */
+    interface ICheckbox
+    {
+        void Paint();
+    }
+}
