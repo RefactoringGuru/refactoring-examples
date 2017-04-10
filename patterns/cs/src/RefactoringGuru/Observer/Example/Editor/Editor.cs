f97764a642aaa504f5427333fad250c3c7a56ce9
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-04-10 21:12:04 +0300

    Observer (#95)
    * Add observer example in csharp
    
    * Move main class to example folder

---
+++ patterns/cs/src/RefactoringGuru/Observer/Example/Editor/Editor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using RefactoringGuru.Observer.Example.Publisher;
+
+namespace RefactoringGuru.Observer.Example.Editor
+{
+    class Editor
+    {
+        public EventManager events;
+        private FileInfo file;
+
+        public Editor()
+        {
+            String[] args = { "open", "save" };
+            this.events = new EventManager(args);
+        }
+
+        public void OpenFile(String filePath)
+        {
+            this.file = new FileInfo(filePath);
+            events.Notify("open", file);
+        }
+
+        public void SaveFile()
+        {
+        if (this.file != null) {
+                events.Notify("save", file);
+            } else {
+                throw new Exception("Please open a file first.");
+            }
+        }
+    }
+}
---
+++ patterns/cs/src/RefactoringGuru/Observer/Example/Listeners/EmailNotificationListener.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace RefactoringGuru.Observer.Example.Listeners
+{
+    class EmailNotificationListener : EventListener
+    {
+        private String email;
+
+        public EmailNotificationListener(String email)
+        {
+            this.email = email;
+        }
+        public void Update(string eventType, FileInfo file)
+        {
+            Console.WriteLine("Email to " + email + ": Someone has performed " + eventType + " operation with the following file: " + file.Name);
+        }
+    }
+}
