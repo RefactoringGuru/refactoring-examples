adfe281f651dcbf352f327c4543b361985de0705
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-04-10 12:48:47 +0300

    Add facade pattern in csharp (#93)
    * Add facade pattern in csharp
    
    * Save changes to Facade.csproj

---
+++ patterns/cs/src/RefactoringGuru/Facade/Example/SomeComplexMediaLibrary/CodecFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RefactoringGuru.Facade.Example.SomeComplexMediaLibrary
+{
+    class CodecFactory
+    {
+        public static ICodec Extract(VideoFile file)
+        {
+            String type = file.GetCodecType();
+            if (type.Equals("mp4"))
+            {
+                Console.WriteLine("CodecFactory: extracting mpeg audio...");
+                return new MPEG4CompressionCodec();
+            }
+            else
+            {
+                Console.WriteLine("CodecFactory: extracting ogg audio...");
+                return new OggCompressionCodec();
+            }
+        }
+    }
+}
---
+++ patterns/cs/src/RefactoringGuru/Facade/Example/SomeComplexMediaLibrary/VideoFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RefactoringGuru.Facade.Example.SomeComplexMediaLibrary
+{
+    class VideoFile
+    {
+        private String name;
+        private String codecType;
+
+        public VideoFile(String name)
+        {
+            this.name = name;
+            int index = name.IndexOf('.');
+            int length = name.Length - (name.IndexOf('.') + 1);
+            codecType = name.Substring(index, length);
+        }
+
+        public String GetCodecType()
+        {
+            return codecType;
+        }
+
+        public String GetName()
+        {
+            return name;
+        }
+    }
+}
