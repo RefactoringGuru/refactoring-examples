bf73eff2489a4a81125e41b15012952e1a25b6aa
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-04-10 21:12:16 +0300

    Add proxy example in csharp (#94)

---
+++ patterns/cs/src/RefactoringGuru/Proxy/Example/SomeCoolMediaLibrary/ThirdPartyYoutubeLib.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RefactoringGuru.Proxy.Example.SomeCoolMediaLibrary
+{
+    interface ThirdPartyYoutubeLib
+    {
+        Dictionary<String, Video> PopularVideos();
+
+        Video GetVideo(String videoId);
+    }
+}
