8347310fd6790d5c6d2d83803620d040df3a1701
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-04-10 13:07:38 +0300

    Add proxy example in csharp

---
+++ patterns/cs/src/RefactoringGuru/Proxy/Example/Downloader/YoutubeDownloader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using RefactoringGuru.Proxy.Example.SomeCoolMediaLibrary;
+
+namespace RefactoringGuru.Proxy.Example.Downloader
+{
+    class YoutubeDownloader
+    {
+        private ThirdPartyYoutubeLib api;
+
+        public YoutubeDownloader(ThirdPartyYoutubeLib api)
+        {
+            this.api = api;
+        }
+
+        public void RenderVideoPage(String videoId)
+        {
+            Video video = api.GetVideo(videoId);
+            Console.WriteLine("\n-------------------------------");
+            Console.WriteLine("Video page (imagine fancy HTML)");
+            Console.WriteLine("ID: " + video.id);
+            Console.WriteLine("Title: " + video.title);
+            Console.WriteLine("Video: " + video.data);
+            Console.WriteLine("-------------------------------\n");
+        }
+
+        public void RenderPopularVideos()
+        {
+            Dictionary<String, Video> list = api.PopularVideos();
+            Console.WriteLine("\n-------------------------------");
+            Console.WriteLine("Most popular videos on Youtube (imagine fancy HTML)");
+            foreach (var VARIABLE in list)
+            {
+                Video video = VARIABLE.Value;
+                Console.WriteLine("ID: " + video.id + " / Title: " + video.title);
+            }
+            Console.WriteLine("-------------------------------\n");
+        }
+    }
+}
---
+++ patterns/cs/src/RefactoringGuru/Proxy/Example/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using RefactoringGuru.Proxy.Example.Downloader;
+using RefactoringGuru.Proxy.Example.Proxy;
+using RefactoringGuru.Proxy.Example.SomeCoolMediaLibrary;
+
+namespace RefactoringGuru.Proxy
+{
+    class Program
+    {
+        private static readonly DateTime Jan1st1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        static void Main(string[] args)
+        {
+            YoutubeDownloader naiveDownloader = new YoutubeDownloader(new ThirdPartyYoutubeClass());
+            YoutubeDownloader smartDownloader = new YoutubeDownloader(new YoutubeCacheProxy());
+
+            long naive = Test(naiveDownloader);
+            long smart = Test(smartDownloader);
+            Console.WriteLine("Time saved by caching proxy: " + (naive - smart) + "ms");
+            Console.ReadKey();
+        }
+
+        public static long Test(YoutubeDownloader downloader)
+        {
+            long startTime = CurrentTimeMillis();
+
+            // User behavior in our app:
+            downloader.RenderPopularVideos();
+            downloader.RenderVideoPage("catzzzzzzzzz");
+            downloader.RenderPopularVideos();
+            downloader.RenderVideoPage("dancesvideoo");
+            // Looks like out users visit same pages very often.
+            downloader.RenderVideoPage("catzzzzzzzzz");
+            downloader.RenderVideoPage("someothervid");
+
+            long estimatedTime = CurrentTimeMillis() - startTime;
+            Console.WriteLine("Time elapsed: " + estimatedTime + "ms\n");
+            return estimatedTime;
+        }
+
+        public static long CurrentTimeMillis()
+        {
+            return (long)(DateTime.UtcNow - Jan1st1970).TotalMilliseconds;
+        }
+    }
+}
