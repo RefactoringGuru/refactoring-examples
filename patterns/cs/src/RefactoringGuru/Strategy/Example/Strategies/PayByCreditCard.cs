59f2ca537bf2f1cc36c6abbe1f8fb536913bab41
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-04-11 12:15:18 +0300

    Add strategy example in csharp

---
+++ patterns/cs/src/RefactoringGuru/Strategy/Example/Strategies/PayByCreditCard.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace RefactoringGuru.Strategy.Example.Strategies
+{
+    /**
+     * EN: Concrete strategy. Implements credit card payment method.
+     * 
+     * RU: Конкретная стратегия. Реализует оплату корзины интернет магазина
+     * кредитной картой клиента.
+     */
+    class PayByCreditCard : PayStrategy
+    {
+        private CreditCard card;
+
+        /**
+         * EN: Collect credit card data.
+         * 
+         * RU: Собираем данные карты клиента.
+         */
+        public void CollectPaymentDetails()
+        {
+            Console.WriteLine("Enter card number: ");
+            String number = Console.ReadLine();
+            Console.WriteLine("Enter date 'mm/yy': ");
+            String date = Console.ReadLine();
+            Console.WriteLine("Enter cvv code: ");
+            String cvv = Console.ReadLine();
+            card = new CreditCard(number, date, cvv);
+
+            // EN: Validate the card number.
+            //
+            // RU: Валидируем номер карты.
+        }
+
+        /**
+         * EN: After card validation we can charge customer's credit card.
+         * 
+         * RU: После проверки карты мы можем совершить оплату. Если клиент
+         * продолжает покупки, мы не запрашиваем карту заново.
+         */
+        public bool Pay(int paymentAmount)
+        {
+            if (CardIsPresent())
+            {
+                Console.WriteLine("Paying " + paymentAmount + " using Credit Card");
+                card.SetAmount(card.GetAmount() - paymentAmount);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool CardIsPresent()
+        {
+            return card != null;
+        }
+
+    }
+}
---
+++ patterns/cs/src/RefactoringGuru/Strategy/Example/Strategies/PayByPayPal.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RefactoringGuru.Strategy.Example.Strategies
+{
+    /**
+     * EN: Concrete strategy. Implements PayPal payment method.
+     * 
+     * RU: Конкретная стратегия. Реализует оплату корзины интернет магазина через
+     * платежную систему PayPal.
+     */
+    class PayByPayPal : PayStrategy
+    {
+        private static Dictionary<String, String> DATA_BASE = new Dictionary<string, string>();
+        private String email;
+        private String password;
+        private bool signedIn;
+
+        public PayByPayPal()
+        {
+            FillDataBase();
+        }
+
+        /**
+         * EN: Collect customer's data.
+         * 
+         * RU: Собираем данные от клиента.
+         */
+        public void CollectPaymentDetails()
+        {
+            while (!signedIn)
+            {
+                Console.Write("Enter user email: ");
+                email = Console.ReadLine();
+                Console.Write("Enter password: ");
+                password = Console.ReadLine();
+                if (Verify())
+                {
+                    Console.WriteLine("Data verification was successful");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong email or password!");
+                }
+            }
+        }
+
+        private bool Verify()
+        {
+            SetSignedIn(email.Equals(DATA_BASE[password]));
+            return signedIn;
+        }
+
+        /**
+         * EN: Save customer data for future shopping attempts.
+         * 
+         * RU: Если клиент уже вошел в систему, то для следующей оплаты данные
+         * вводить не придется.
+         */
+        public bool Pay(int paymentAmount)
+        {
+            if (signedIn)
+            {
+                Console.WriteLine("Paying " + paymentAmount + " using PayPal");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void SetSignedIn(bool signedIn)
+        {
+            this.signedIn = signedIn;
+        }
+
+        private void FillDataBase()
+        {
+            DATA_BASE.Add("amanda1985", "amanda@ya.com");
+            DATA_BASE.Add("qwerty", "john@amazon.eu");
+        }
+    }
+}
