2d640ee71a1a797d624f66df837cfd6af7dcc51e
Bogdan Gerashchenko <b.gerashchencko@gmail.com>
2017-04-12 13:14:59 +0300

    add visitor example in csharp (#99)

---
+++ patterns/cs/src/RefactoringGuru/Visitor/Example/Shapes/CompoundShape.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using RefactoingGuru.Visitor.Example.Shapes;
+using RefactoingGuru.Visitor.Example.Visitor;
+
+namespace RefactoingGuru.Visitor.Example.Shapes
+{
+    class CompoundShape : Shape
+    {
+        public int id;
+        public List<Shape> children = new List<Shape>();
+
+        public CompoundShape(int id)
+        {
+            this.id = id;
+        }
+
+        public void Move(int x, int y)
+        {
+            // move shape
+        }
+
+        public void Draw()
+        {
+            // draw shape
+        }
+
+        public int GetId()
+        {
+            return id;
+        }
+
+        public string Accept(IVisitor visitor)
+        {
+            return visitor.VisitCompoundGraphic(this);
+        }
+
+        public void Add(Shape shape)
+        {
+            children.Add(shape);
+        }
+    }
+}
---
+++ patterns/cs/src/RefactoringGuru/Visitor/Example/Shapes/Shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using RefactoingGuru.Visitor.Example.Visitor;
+
+namespace RefactoingGuru.Visitor.Example.Shapes
+{
+    interface Shape
+    {
+        void Move(int x, int y);
+        void Draw();
+        String Accept(IVisitor visitor);
+    }
+}
