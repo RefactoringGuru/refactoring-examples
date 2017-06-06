e603246f48436f9ac72c3310d9d3e2a2d3e17018
Alexander Shvets <neochief@shvetsgroup.com>
2017-06-06 19:57:53 +0300

    Added new Flyweight content.

--- patterns/pseudocode/src/flyweight.js
+++ patterns/pseudocode/src/flyweight.js
@@ -1,12 +1,13 @@
-// EN: This flyweight class contains part of the fields, which describe a tree.
-// These fields values are not unique for each tree; you won't find tree
-// coordinates there. But several trees can have the same texture. And since
-// that texture is usually BIG, you're wasting a lot of memory by keeping it
-// million times for similar trees. That's why we extract texture to a separate
-// object, which can be referenced from all those similar trees.
+// EN: The Flyweight class contains only a portion of state that describes a
+// tree. These field store values that hardly unique for each particular tree.
+// You won't find here tree coordinates, however textures and colors shared
+// between multiple are here. And since this data is usually BIG, you'd waste a
+// lot of memory by keeping it in each tree object. That's why we extract
+// texture, colors and other data to a separate flyweight class that can be
+// referenced from all those similar trees.
 // 
 // RU: Этот класс-легковес содержит часть полей, которые описывают деревья. Эти
-// поля не уникальные для каждого дерева как, например, координаты. Например,
+// поля не уникальные для каждого дерева как, скажем, координаты. Например,
 // несколько деревьев могут иметь ту же текстуру. Поэтому мы переносим
 // повторяющиеся данные в один единственный объект и ссылаемся на него из
 // конкретных деревьев.
@@ -19,8 +20,8 @@ class TreeType is
         Create a bitmap from type, color and texture.
         Draw bitmap on canvas at X and Y.
 
-// EN: Flyweight factory decides when to re-use existing flyweight and when to
-// create a new one.
+// EN: Flyweight factory decides whether to re-use existing flyweight or create
+// a new object.
 // 
 // RU: Фабрика легковесов решает когда нужно создать новый легковес, а когда
 // можно обойтись существующим.
@@ -33,9 +34,9 @@ class TreeFactory is
             treeTypes.add(tree)
         return tree
 
-// EN: Context object keeps unique part of tree's state. Application can create
-// billions of these since they are pretty thin–just two integer coordinates and
-// one reference.
+// EN: Context object contains extrinsic part of tree state. Application can
+// create billions of these since they are pretty thin: just two integer
+// coordinates and one reference.
 // 
 // RU: Контекстный объект, из которого мы выделили легковес TreeType. В
 // программе могут быть тысячи объектов Tree, так как накладные расходы на их
@@ -51,7 +52,7 @@ class Tree is
 // EN: Tree and Forest classes are Flyweight's clients. You might merge them
 // together if you don't plan to develop a Tree class any further.
 // 
-// RU: Классы Tree и Forest являются клиентами легковеса. При желании их можно
+// RU: Классы Tree и Forest являются клиентами Легковеса. При желании их можно
 // слить в один класс, если класс Tree нет нужды расширять далее.
 class Forest is
     field trees: collection of Trees
