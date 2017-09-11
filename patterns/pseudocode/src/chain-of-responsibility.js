3bc375e050355e8c76ed4a472cca100ff3500d13
Alexander Shvets <neochief@shvetsgroup.com>
2017-09-11 19:45:07 +0300

    Updated CoR pattern.

--- patterns/pseudocode/src/chain-of-responsibility.js
+++ patterns/pseudocode/src/chain-of-responsibility.js
@@ -1,24 +1,29 @@
 // EN: Handler interface.
-//
+// 
 // RU: Интерфейс обработчиков.
 interface ComponentWithContextualHelp is
     method showHelp() is
 
 
 // EN: Base class for simple components.
-//
+// 
 // RU: Базовый класс простых компонентов.
 abstract class Component implements ContextualHelp is
     field tooltipText: string
 
-    // EN: Container, which contains component, severs as a following object in chain.
-    //
-    // RU: Контейнер, содержащий компонент, служит в качестве следующего звена цепочки.
+    // EN: Container, which contains component, severs as a following object
+    // in chain.
+    // 
+    // RU: Контейнер, содержащий компонент, служит в качестве следующего
+    // звена цепочки.
     protected field container: ContainerComponent
 
-    // EN: Component shows tooltip if there's a help text assigned to it. Otherwise it forwards the call to the container if it exists.
-    //
-    // RU: Компонент показывает всплывающую подсказку, если задан текст подсказки. В обратном случае, он перенаправляет запрос контейнеру, если тот существует.
+    // EN: Component shows tooltip if there's a help text assigned to it.
+    // Otherwise it forwards the call to the container if it exists.
+    // 
+    // RU: Компонент показывает всплывающую подсказку, если задан текст
+    // подсказки. В обратном случае, он перенаправляет запрос контейнеру, если
+    // тот существует.
     method showHelp() is
         if (tooltipText != null)
             Show tooltip.
@@ -26,9 +31,13 @@ abstract class Component implements ContextualHelp is
             container.showHelp();
 
 
-// EN: Containers can contain both simple components and other container as children. The chain relations are established here. The class inherits showHelp behavior from its parent.
-//
-// RU: Контейнеры могут включать в себя как простые компоненты, так и другие контейнеры. Здесь формируются связи цепочки. Класс унаследует метод showHelp от своего родителя.
+// EN: Containers can contain both simple components and other container as
+// children. The chain relations are established here. The class inherits
+// showHelp behavior from its parent.
+// 
+// RU: Контейнеры могут включать в себя как простые компоненты, так и другие
+// контейнеры. Здесь формируются связи цепочки. Класс унаследует метод showHelp
+// от своего родителя.
 abstract class ContainerComponent extends Component is
     protected field children: array of Component
 
@@ -38,14 +47,18 @@ abstract class ContainerComponent extends Component is
 
 
 // EN: Primitive components may be fine with default help implementation...
-//
+// 
 // RU: Примитивные компоненты может устраивать поведение помощи по умолчанию...
 class Button extends Component is
     // ...
 
-// EN: But complex components may override the default implementation. If a help can not be provided in a new way, the component can always call the base implementation (see Component class).
-//
-// RU: Но сложные компоненты могут переопределять метод помощь по-своему. Но если помощь не может быть предоставлена, компонент вызовет базовую реализацию (см. класс Component)
+// EN: But complex components may override the default implementation. If a help
+// can not be provided in a new way, the component can always call the base
+// implementation (see Component class).
+// 
+// RU: Но сложные компоненты могут переопределять метод помощь по-своему. Но
+// если помощь не может быть предоставлена, компонент вызовет базовую реализацию
+// (см. класс Component)
 class Panel extends ContainerComponent is
     field modalHelpText: string
 
@@ -56,7 +69,7 @@ class Panel extends ContainerComponent is
             parent::showHelp()
 
 // EN: ...same as above...
-//
+// 
 // RU: ...то же, что и выше...
 class Dialog extends ContainerComponent is
     field wikiPage: string
@@ -69,11 +82,11 @@ class Dialog extends ContainerComponent is
 
 
 // EN: Client code.
-//
+// 
 // RU: Клиентский код.
 class Application is
     // EN: Each application configures the chain differently.
-    //
+    // 
     // RU: Каждое приложение конфигурирует цепочку по-своему.
     method createUI() is
         dialog = new Dialog("Budget Reports")
@@ -89,7 +102,7 @@ class Application is
         dialog.add(panel)
 
     // EN: Imagine what happens here.
-    //
+    // 
     // RU: Представьте что здесь произойдёт.
     method onF1KeyPress() is
         component = this.getComponentAtMouseCoords()
