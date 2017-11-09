5a98719a0c2b311a2548b47a7855dc6a29799512
Alexander Shvets <neochief@shvetsgroup.com>
2017-11-09 16:59:37 +0200

    Fix.

--- simple/php/replace-error-code-with-exception_after.php
+++ simple/php/replace-error-code-with-exception_after.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * (Here you put some real function documentation.
- *  Note: the line below indicates that function
- *  can throw an exception of given type.)
- * @throws BalanceException
- */
 function withdraw($amount) {
   if ($amount > $this->balance) {
     throw new BalanceException();
