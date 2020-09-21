7fbf9db63c6c2a0cfb3211bcb3b755e4ab9d9a5d
Alexander Shvets <neochief@shvetsgroup.com>
2020-09-21 17:32:19 +0300

    TS assertions.

--- simple/typescript/introduce-assertion_after.ts
+++ simple/typescript/introduce-assertion_after.ts
@@ -1,4 +1,11 @@
 getExpenseLimit(): number {
+  // TypeScript and JS doesn't have built-in assertions, so we'll use
+  // good-old console.error(). You can always extract this into a
+  // designated assertion function.
+  if (!(expenseLimit != NULL_EXPENSE ||
+       (typeof primaryProject !== 'undefined' && primaryProject))) {
+      console.error("Assertion failed: getExpenseLimit()");
+  }
 
   return (expenseLimit != NULL_EXPENSE) ?
     expenseLimit:
